e936b1c4a513ce2f536433de7bb497d700475263
Luan009 <115900387+Luan009@users.noreply.github.com>
2023-01-08 16:38:44 +0100

    Delete HW8/Task_1 directory

--- HW8/Task_1/DebuggingExample/src/de/uni/koeln/sse/se/Month.java
+++
@@ -1,9 +0,0 @@
-package de.uni.koeln.sse.se;
-
-public enum Month {
-	
-	JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, 
-	JULY, AUGUST, SEPTEMBER, OCTOBER , NOVEMBER, DECEMBER
-
-
-}
--- HW8/Task_1/README.md
+++
@@ -1,2 +0,0 @@
-## HW8: Debugging and Testing  1:
-### Task1: Use the debugging tool of your IDE to debug this code sample as explained in the task sheet of HW8
